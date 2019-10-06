15bda836f30d4876dbe7dc0fb2df396544ad3000
LOMOKIYO <am6966@i.softbank.jp>
2019-10-06 00:34:04 -0700

    modified

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,6 +1,5 @@
 class MessageController < ApplicationController
   def index
-    
   end
 end
 
--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module ChatSpace
@@ -14,9 +13,5 @@ module ChatSpace
       g.helper false
       g.test_framework false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
