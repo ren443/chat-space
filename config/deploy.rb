d1c43dba112e3442a2ecec28812188b95be86876
ren <ren443te@gmail.com>
2019-10-24 20:55:30 +0900

    deploy5

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:ren/chat-space.git"
+set :repo_url, "git@github.com:ren443/chat-space.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
